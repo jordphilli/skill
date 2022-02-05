728d693f885c5a6c9bccf36b0fc7a72ad6db3c8d
jordphilli <97648396+jordphilli@users.noreply.github.com>
2022-02-04 16:30:20 -0800

    Update SkillTester.java

--- SkillTester.java
+++ SkillTester.java
@@ -3,6 +3,6 @@ import org.junit.*;
 public class SkillTester {
     @Test
     public void addition() {
-        assertEquals(2, Skill.add(1,1));
+        assertEquals(2, Skill.add(1,1));//should pass now
     }
 }
